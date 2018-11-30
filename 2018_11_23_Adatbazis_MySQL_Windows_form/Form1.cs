d46465f96611064ad24c107ac5db569b8c9598dd
bartab9610 <barnabas96@freemail.hu>
2018-11-30 12:29:33 +0100

    Adatbázis létrehozás - Hibakezelés

--- 2018_11_23_Adatbazis_MySQL_Windows_form/Form1.cs
+++ 2018_11_23_Adatbazis_MySQL_Windows_form/Form1.cs
@@ -13,9 +13,33 @@ namespace _2018_11_23_Adatbazis_MySQL_Windows_form
 {
     public partial class Form_adatbazis : Form
     {
+        const string Adatbazis_letrehozas_SQL = @"CREATE TABLE IF NOT EXISTS felhasznalo 
+                                                    (
+                                                        felhasznalo_id INTEGER AUTO_INCREMENT PRIMARY KEY,
+                                                        felhasznalo_nev VARCHAR(80) NOT NULL UNIQUE,
+                                                        felhasznalo_jelszo VARCHAR(100) NOT NULL,
+                                                        felhasznalo_reg_datum DATE NOT NULL
+                                                    )";
+        MySqlConnection conn;
+
         public Form_adatbazis()
         {
             InitializeComponent();
+            try
+            {
+                conn = new MySqlConnection("Server=localhost;Database=regisztracio;Uid=root;Pwd=;");
+                conn.Open();
+                // TÁBLA ELLENŐRZÉSE HOGY LÉTRE VAN-E HOZVA
+                var letrehozas = conn.CreateCommand();
+                letrehozas.CommandText = Adatbazis_letrehozas_SQL;
+                letrehozas.ExecuteNonQuery();
+                // --------------------
+            }
+            catch (MySqlException ex)
+            {
+                MessageBox.Show("Adatbázis hiba: " + ex.Message);
+                this.Close();
+            }
         }
 
         private void Button_regisztralas_Click(object sender, EventArgs e)
@@ -25,9 +49,8 @@ namespace _2018_11_23_Adatbazis_MySQL_Windows_form
             string jelszo = TextBox_jelszo.Text;
             DateTime szuletesi_datum = DateTimePicker_szul_ido.Value;
             // https://www.connectionstrings.com/mysql/
-            using (var conn = new MySqlConnection("Server=localhost;Database=regisztracio;Uid=root;Pwd=;")) // Ezzel működik -> using MySql.Data.MySqlClient;
+            // using (var conn = new MySqlConnection("Server=localhost;Database=regisztracio;Uid=root;Pwd=;")) // Ezzel működik -> using MySql.Data.MySqlClient;
             {
-                conn.Open();
                 // HIBAKEZELÉS -------
                 var ellenorzes = conn.CreateCommand();
                 ellenorzes.CommandText = "SELECT COUNT(*) FROM felhasznalo WHERE felhasznalo_nev = @nev";
@@ -46,6 +69,7 @@ namespace _2018_11_23_Adatbazis_MySQL_Windows_form
                 command.Parameters.AddWithValue("@jelszo", jelszo);
                 command.Parameters.AddWithValue("@regdatum", szuletesi_datum);
                 int erintettSorok = command.ExecuteNonQuery();
+                // -------------------------------
             }
         }
         private void Button_torles_Click(object sender, EventArgs e)
@@ -70,7 +94,7 @@ namespace _2018_11_23_Adatbazis_MySQL_Windows_form
                     torles.CommandText = "DELETE FROM felhasznalo WHERE felhasznalo_nev = @nev";
                     torles.Parameters.AddWithValue("@nev", nev);
                     int erintettSorok = torles.ExecuteNonQuery();
-                    MessageBox.Show("2!");
+                    MessageBox.Show("Felhasználó törlésre került!");
                     return;
                 }
                 // --------------------
